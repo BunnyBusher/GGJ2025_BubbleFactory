03ae1068564b0968056925a5507286a6ca31c089
NicolasCarlier <n.carlier6000@gmail.com>
2025-01-22 16:11:55 +0100

    fin des script mais debug en cours

--- Assets/_/Scripts/Factory/Production.cs
+++ Assets/_/Scripts/Factory/Production.cs
@@ -1,50 +1,55 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Production : MonoBehaviour
+public class Production : StockInventory
 {
     
-    [System.Serializable]
-    public class Recipe
-    {
-        [SerializeField] private string _recipeName;
-        public List<Ingredients> ingredient;
-    }
-    [System.Serializable]
-    public class Ingredients
-    {
-        public GatherableScriptableObject ressourceData;
-        public int numberOfIngredient;
-    }
+    //[System.Serializable]
+    //public class Recipe
+    //{
+    //    [SerializeField] private string _recipeName;
+    //    public List<Ingredients> ingredient;
+    //}
+    //[System.Serializable]
+    //public class Ingredients
+    //{
+    //    public GatherableScriptableObject ressourceData;
+    //    public int numberOfIngredient;
+    //}
 
-    [SerializeField] private Recipe _recipe;
+    //[SerializeField] private Recipe _recipe;
 
     [Header ("Factory parameters")]
     [SerializeField] private GatherableScriptableObject _product;
     [SerializeField] private int _numberOfProduct;
     [SerializeField] private float _timeToProduct;
 
-    private StockInventory _stock;
-    private bool _isRessourceInStock = false;
+          
     private float _currentTime;
 
-    private void Awake()
+    private void FixedUpdate()
     {
-        _stock = GetComponent<StockInventory>();
-    }
-    private void Update()
-    {
-        if (_stock)
-        
         _currentTime += Time.deltaTime;
-        if (_currentTime > _timeToProduct)
+        if ( _currentTime >= _timeToProduct)
         {
-            _currentTime = 0;
-            
+            DoProduction();
         }
+
     }
+
+
     private void DoProduction()
     {
+        foreach (bool b in _isRessourceInStock)
+        {
+            if (!b) return;            
+        }
 
+        for (int i =0; i < _stock.Count; i++)
+        {
+            _stock[i].currentNumberOfRessource -= _stock[i].numberNeededToProduce;
+        }
+        Debug.Log("Oxygene crée");
     }
 }
--- Assets/_/Scripts/Factory/StockInventory.cs
+++ Assets/_/Scripts/Factory/StockInventory.cs
@@ -6,33 +6,43 @@ public class StockInventory : MonoBehaviour
     [System.Serializable]
     public class Stock
     {
-        [SerializeField] private string _stockName;
+        [SerializeField] protected string _stockName;
         public GatherableScriptableObject ressourceData;
         public int maxNumberOfRessource;
         public int currentNumberOfRessource;
+        public int numberNeededToProduce;
     }
 
-    [SerializeField] private List<Stock> _stock;
-    private InventoryManager _inventory;
+    [SerializeField] protected List<Stock> _stock;
+    protected InventoryManager _inventory;
+    public List<bool> _isRessourceInStock;
 
     private void Start()
     {
         _inventory = FindAnyObjectByType<InventoryManager>();
+        _isRessourceInStock = new List<bool>();
+
+        for (int i =0; i < _stock.Count; i++)
+        {
+            _isRessourceInStock.Add(false);
+        }
     }
 
-    public void AddRessource (string tag)
+    public virtual void AddRessource (string tag)
     {
         for (int i = 0; i < _inventory.ressourceSlots.Count;i++)
         {
             if (_inventory.ressourceSlots[i].tag == tag)        //If find a ressource with the same tag in Inventory
             {
-                _inventory.RemoveRessource(i);                  //Remove that ressource from inventory
+                _inventory.RemoveRessource(i);
+                Destroy(_inventory.ressourceSlots[i]);//Remove that ressource from inventory
                 for (int stockIndex = 0; stockIndex < _stock.Count; stockIndex++)   
                 {
                     if (_stock[stockIndex].ressourceData.ressourceNameTag == tag 
                         && _stock[stockIndex].currentNumberOfRessource < _stock[stockIndex].maxNumberOfRessource) // Find the ressource in stock, and check if not at Max Stock
                     {
                         _stock[stockIndex].currentNumberOfRessource++;
+                        if (_stock[stockIndex].currentNumberOfRessource >= _stock[stockIndex].numberNeededToProduce) _isRessourceInStock[stockIndex] = true;
                         return;
                     }
                 }
@@ -41,6 +51,22 @@ public class StockInventory : MonoBehaviour
             }
         }
             
+    }
+
+    public virtual void RemoveRessource(string tag)
+    {
+        for (int i = 0; i < _stock.Count; i++)
+        {
+            if (_stock[i].ressourceData.ressourceNameTag == tag 
+                && _stock[i].currentNumberOfRessource < 0)
+            {
+                _stock[i].currentNumberOfRessource--;
+                if (_stock[i].currentNumberOfRessource < _stock[i].numberNeededToProduce) _isRessourceInStock[i] = false;
+                return;
+            }
+                        
+        }
+        Debug.LogWarning("No ressource in stock");
     }
 
 
